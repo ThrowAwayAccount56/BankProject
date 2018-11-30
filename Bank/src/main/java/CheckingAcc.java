d674b0baa3e6a5e2238ad14a2d11879039e1aac1
ThrowAwayAccount56 <45332596+ThrowAwayAccount56@users.noreply.github.com>
2018-11-30 10:31:10 -0500

    Update CheckingAcc.java
    lotta stuff in here

--- Bank/src/main/java/CheckingAcc.java
+++ Bank/src/main/java/CheckingAcc.java
@@ -1,6 +1,75 @@
-public class CheckingAcc extends BankAcc {
+package BankAccount;
 
-    public boolean isOverDraftProtected(){}
-    public void ChargeOverdraft(SavingsAcc){}
-    public void sendMoney(bankAccMock){};
+import Bank.FinancialTracker;
+import Exceptions.InvalidMoneyAmount;
+
+public class CheckingAccount extends BankAccount {
+    private double  cashInAccount = 0;
+    private boolean overDraftProtection;
+    private final double overDraftFee = 20;
+    public CheckingAccount(FinancialTracker financialTracker, boolean overDraftProtection)
+    {
+        super(financialTracker);
+        this.overDraftProtection = overDraftProtection;
+    }
+
+    @Override
+    public void depositMoney(double amount) throws InvalidMoneyAmount {
+        if(amount > maxExchange) throw new InvalidMoneyAmount();
+        else cashInAccount+=amount;
+    }
+
+    @Override
+    public  void transferMoney(BankAccount recipient, double amount){
+        if(amount > this.cashInAccount && maxDebtAllowed > debt + amount)
+        {
+            if(!isOverDraftProtected()) chargeOverDraftFee();
+            debt = debt + amount - cashInAccount;
+            cashInAccount = 0;
+            financialTracker.addCash(amount);
+        }
+        else if(amount < this.cashInAccount)
+        {
+            recipient.depositMoney(cashInAccount - amount);
+            cashInAccount -= amount;
+            financialTracker.addCash(amount);
+        }
+
+        else{
+            throw new InvalidMoneyAmount();
+        }
+
+    }
+
+    @Override
+    public void updateAccount(){
+        if(!IsMinumumReached()) cashInAccount =- minimumCharge;
+    }
+
+    @Override
+    public boolean IsMinumumReached(){
+        if(this.cashInAccount < minimumAccountBalance) return false;
+        return true;
+    }
+
+    @Override
+    public double getMoneyInAccount() {
+        return this.cashInAccount;
+    }
+
+    @Override
+    public void depositMoneyUsingCheck(String bankRoutingNumber, String checkAccountNumber, double amount) throws InvalidMoneyAmount{
+        if(amount > maxExchange || !isCheckNumberValid(bankRoutingNumber, checkAccountNumber)) throw new InvalidMoneyAmount();
+        else cashInAccount+=amount;
+    }
+
+    private void chargeOverDraftFee(){
+        cashInAccount -= overDraftFee;
+    }
+
+    private boolean isOverDraftProtected(){
+        if(overDraftProtection != false) return true;
+        return false;
+    }
+
 }
