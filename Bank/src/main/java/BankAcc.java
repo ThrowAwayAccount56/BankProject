b897bd707d6f40bdce7d9ae953d30c89646a1da2
ThrowAwayAccount56 <45332596+ThrowAwayAccount56@users.noreply.github.com>
2018-11-30 10:22:50 -0500

    Update BankAcc.java

--- Bank/src/main/java/BankAcc.java
+++ Bank/src/main/java/BankAcc.java
@@ -1,12 +1,33 @@
-public class BankAcc {
+import Bank.FinancialTracker;
 
-    protected static double debt;
-    private double balance;
+import Exceptions.InvalidMoneyAmount;
 
-    public class bankAcc(Customer customer){}
-    public class depositMoney(double money) throws InvalidCheckNumberException{}
-    public class transfer(BankAcc){}
-    public class viewMoney(BankAcc){}
-    public class boolean isMinimumReached(BankAcc){}
-    public class boolean isCheckNumberValid(int checkNumber){}
-}
+//maybe make this an abstract class?
+public abstract class BankAccount {
+    protected  double minimumAccountBalance = 1000; // an account must have at least 1,000$ as to not be charged.
+    protected  double minimumCharge = 20; // if they don't charge the account
+    protected  double maxExchange = 20000; // the bank will only accept a deposit of 20,000$
+    protected FinancialTracker financialTracker;
+    protected double debt = 0;
+    protected double maxDebtAllowed = 20000;
+
+    public BankAccount(FinancialTracker financialTracker){
+        this.financialTracker = financialTracker;
+    }
+    public abstract void depositMoney(double amount) throws InvalidMoneyAmount;
+    public abstract void depositMoneyUsingCheck(String bankRoutingNumber, String checkAccountNumber, double amount) throws InvalidMoneyAmount;
+    public abstract void transferMoney(BankAccount recipient, double amount);
+
+    public abstract double getMoneyInAccount();
+
+    public abstract void updateAccount(); // needs to implemented by each subclass. in progress
+
+    public double getMinimumCharge() {return minimumAccountBalance;}
+    public abstract boolean IsMinumumReached();
+
+    public boolean isCheckNumberValid(String bankRoutingNumber, String checkAcountNumber) {
+     if(bankRoutingNumber.length() != 9 || checkAcountNumber.length() > 17 || checkAcountNumber.length() < 0) return false;
+     return true;
+    }
+
+
